6572abb7e59e1b940b5bec6df089626ddd94d2c2
bharris62 <blakebharris@gmail.com>
2017-01-04 21:25:01 -0600

    christmas trees!

--- week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
+++ week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
@@ -1,19 +1,59 @@
+
 public class PrintingLikeBoss {
 
     // copy or rewrite the method of Assignment 39.1 here
     public static void printStars(int amount) {
+        while (amount > 0) {
+            System.out.printf("*");
+            amount--;
+        }
+        System.out.printf("\n");
     }
 
     public static void printWhitespaces(int amount) {
         // 40.1
+        while (amount > 1) {
+            System.out.printf(" ");
+            amount--;
+        }
+
     }
 
     public static void printTriangle(int size) {
         // 40.2
+        int numberStars = 1;
+        int num = 0;
+        int triangleSize = size;
+
+        while(num < triangleSize) {
+            printWhitespaces(size);
+            printStars(numberStars);
+            num++;
+            size--;
+            numberStars++;
+        }
     }
 
     public static void xmasTree(int height) {
         // 40.3
+        int treeSize = height;
+        int star = 1;
+        int treeHeight = 0;
+
+        while(treeHeight < treeSize){
+            printWhitespaces(height);
+            printStars(star);
+            star+=2;
+            height--;
+            treeHeight++;
+        }
+        int x = 0;
+        while(x < 2) {
+            printWhitespaces(treeSize-1);
+            printStars(3);
+            x++;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -24,5 +64,6 @@ public class PrintingLikeBoss {
         xmasTree(4);
         System.out.println("---");
         xmasTree(10);
+
     }
 }
