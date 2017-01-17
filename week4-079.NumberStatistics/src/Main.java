88f22283a22a335408e976c01ec3eea8dd86e066
bharris62 <blakebharris@gmail.com>
2017-01-17 14:46:41 -0600

    solved number statistics problem

--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        NumberStatistics stats = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
+
         Scanner reader = new Scanner(System.in);
-        // Write test code here
-        // Remember to remove all the extra code when doing assignments  79.3 and 79.4
+        System.out.println("Type numbers: ");
 
-        // Define three NumberStatistics objects in your program:
-        // The first is used to track the sum of all given numbers.
-        // The second takes care of even numbers and the third the odd numbers.
+        while(true) {
+            int number = Integer.parseInt(reader.nextLine());
+            if (number == -1) {
+                break;
+            } else if (number % 2 == 0) {
+                even.addNumber(number);
+                stats.addNumber(number);
+            } else {
+                odd.addNumber(number);
+                stats.addNumber(number);
+            }
 
-        // The tests does not work if you do not create the objects in the correct order
 
+        }
+        System.out.println("sum: " + stats.sum());
+        System.out.println("sum of even: " + even.sum());
+        System.out.println("sum of odd: " + odd.sum());
     }
 }
--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,3 +1,33 @@
 
 public class NumberStatistics {
+    private int amountOfNumbers;
+    private int totalSum;
+
+    public NumberStatistics(){
+        this.amountOfNumbers = 0;
+        this.totalSum = 0;
+
+    }
+
+    public void addNumber(int number){
+        amountOfNumbers++;
+        totalSum += number;
+    }
+
+    public int amountOfNumbers(){
+
+        return amountOfNumbers;
+    }
+
+    public int sum(){
+        return totalSum;
+    }
+
+    public double average(){
+        if (amountOfNumbers == 0) {
+            return 0;
+        }
+        return (double)totalSum / amountOfNumbers;
+
+    }
 }
