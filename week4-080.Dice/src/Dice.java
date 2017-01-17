946a89d7dfd2c56f0f9973b7acb90efb116f9b70
bharris62 <blakebharris@gmail.com>
2017-01-17 15:48:36 -0600

    random done

--- week4-080.Dice/src/Dice.java
+++ week4-080.Dice/src/Dice.java
@@ -6,10 +6,12 @@ public class Dice {
 
     public Dice(int numberOfSides) {
         // Initialize here the number of sides
+        this.numberOfSides = numberOfSides;
+        random = new Random();
     }
 
     public int roll() {
-        // create here a random number belongig to range 1-numberOfSided
-        return 0;
+        // create here a random number belonging to range 1-numberOfSided
+        return this.random.nextInt(numberOfSides) + 1;
     }
 }
--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -2,13 +2,21 @@ import java.util.Random;
 
 public class PasswordRandomizer {
     // Define the variables
+    private int length;
+    private String abc = "abcdefghijklmnopqrstuvwxyz";
+    Random random = new Random;
 
     public PasswordRandomizer(int length) {
         // Initialize the variable
+        this.length = length;
     }
 
     public String createPassword() {
         // write code that returns a randomized password
+        String randomPass = "";
+        while(this.length > 0){
+            randomPass+=
+        }
         return "";
     }
 }
