6c03e85a06850c2482e0b91e3ea05d5788201d3c
bharris62 <blakebharris@gmail.com>
2017-01-17 15:52:40 -0600

    random password generator

--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -4,19 +4,22 @@ public class PasswordRandomizer {
     // Define the variables
     private int length;
     private String abc = "abcdefghijklmnopqrstuvwxyz";
-    Random random = new Random;
+    Random random = new Random();
 
     public PasswordRandomizer(int length) {
         // Initialize the variable
         this.length = length;
+        random = new Random();
     }
 
     public String createPassword() {
         // write code that returns a randomized password
         String randomPass = "";
-        while(this.length > 0){
-            randomPass+=
+        int i = 0;
+        while(i < this.length){
+            randomPass += this.abc.charAt(random.nextInt(abc.length()));
+            i++;
         }
-        return "";
+        return randomPass;
     }
 }
--- week4-081.PasswordRandomizer/src/Program.java
+++ week4-081.PasswordRandomizer/src/Program.java
@@ -2,8 +2,8 @@ public class Program {
     public static void main(String[] args) {
         PasswordRandomizer randomizer = new PasswordRandomizer(13);
         System.out.println("Password: " + randomizer.createPassword());
-        System.out.println("Password: " + randomizer.createPassword());
-        System.out.println("Password: " + randomizer.createPassword());
-        System.out.println("Password: " + randomizer.createPassword());
+//        System.out.println("Password: " + randomizer.createPassword());
+//        System.out.println("Password: " + randomizer.createPassword());
+//        System.out.println("Password: " + randomizer.createPassword());
     }
 }
