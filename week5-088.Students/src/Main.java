7a117a844ba0ae0690a5682562c7a49298a72543
bharris62 <blakebharris@gmail.com>
2017-01-20 12:57:47 -0600

    objects in ArrayLists problems

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -5,6 +5,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         // write here the main program
+        ArrayList<Student> students = new ArrayList();
+
+        while(true) {
+            System.out.print("name: ");
+            String name = reader.nextLine();
+
+            if(name.equals("")){
+                break;
+            } else {
+                System.out.print("studentnumber: ");
+                String studentNumber = reader.nextLine();
+                students.add(new Student(name, studentNumber));
+            }
+        }
+
+        for (Student s : students) {
+            System.out.println(s);
+        }
+
+        System.out.print("Give search term: ");
+        System.out.println("result:");
+        String searcher = reader.nextLine();
+        for(Student s : students) {
+            if(s.getName().contains(searcher)) {
+                System.out.println(s);
+            }
+        }
+
     }
 }
